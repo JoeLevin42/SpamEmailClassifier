893fc7351904746f29344ea262c195a4aedafa71
Yoni <yonilev2021@gmail.com>
2026-07-21 09:18:59 +0300

    Implemented Classifier

--- Models/Classifier.cs
+++ Models/Classifier.cs
@@ -1,10 +1,38 @@
 
 
+using System.Reflection.Emit;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using SpamEmailClassifier.Datarecord;
+
 namespace SpamEmailClassifier.Models
 {
     class Classifier
     {
-
+        public string Predict(NaiveBayesModel model, DataRecord sample)
+        {
+            string bestLabel = "";
+            double bestScore = - 1000;
+            foreach(string label in model.Labels)
+            {
+                double score = model.Priors[label];
+                foreach((string feature, string value) in sample.LineData)
+                {
+                    string key = label + "_" + feature + "_" + value;
+                    string unseenKey = label + "_" + feature;
+                    if (model.ConditionalProbabilities.ContainsKey(key))
+                        score = score * model.ConditionalProbabilities[key];
+                    else
+                        score = score * model.UnseenProbabilities[unseenKey];
+                }
+                   
+                if(score > bestScore)
+                {
+                    bestScore = score;
+                    bestLabel = label;
+                }
+            }
+            return bestLabel;
+        }
     }
 
 }
